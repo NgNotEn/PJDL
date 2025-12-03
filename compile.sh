ee9ae207ab43674f7d5e331082c940ba6f5aaa6d
Jiaqi Wei <wjq.cn@outlook.com>
2025-12-03 23:33:13 +0800

    clear

--- compile.sh
+++
@@ -1,4 +0,0 @@
-rm -rf bin
-mkdir -p bin
-JAVA_FILES=$(find ./src -name "*.java")
-javac -encoding UTF-8 -d bin -sourcepath src/ -classpath lib/\* $JAVA_FILES
--- run.sh
+++
@@ -1,5 +0,0 @@
-cd bin
-
-# java -Dfile.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Xmx256G -Xms256G -classpath .:../lib/* benchmark.BenchAndVerify ../DB_DATA/JOB /opt/data/private/AVPJoin-R/DB_DATA/job/queries
-java -Dfile.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Xmx256G -Xms256G -classpath .:../lib/* benchmark.BenchAndVerify ../DB_DATA/JOB ../../ADOPT-tpch/JOB/queries > ../res/job_benchmark.txt
-# java -Dfile.encoding=UTF-8 -Dconsole.encoding=UTF-8 -Xmx256G -Xms256G -classpath .:../lib/* benchmark.BenchAndVerify ../DB_DATA/TEST1 /opt/data/private/AVPJoin-R/DB_DATA/test_data_1/query
